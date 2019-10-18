f2d1e84e3a811129332e8709d974fe2edfb34974
Pavol Fulop <pavolfulop@gmail.com>
2019-10-18 13:21:16 +0200

    clean template

---
+++ js/ARScene.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+import { StyleSheet } from "react-native";
+
+import { ViroARScene, ViroText, ViroConstants } from "react-viro";
+
+export default class ARScene extends Component {
+  constructor() {
+    super();
+
+    // Set initial state here
+    this.state = {
+      text: "Initializing AR..."
+    };
+
+    // bind 'this' to functions
+    this._onInitialized = this._onInitialized.bind(this);
+  }
+
+  render() {
+    return (
+      <ViroARScene onTrackingUpdated={this._onInitialized}>
+        <ViroText
+          text={this.state.text}
+          scale={[0.5, 0.5, 0.5]}
+          position={[0, 0, -1]}
+          style={styles.helloWorldTextStyle}
+        />
+      </ViroARScene>
+    );
+  }
+
+  _onInitialized(state, reason) {
+    if (state == ViroConstants.TRACKING_NORMAL) {
+      this.setState({
+        text: "Hello World!"
+      });
+    } else if (state == ViroConstants.TRACKING_NONE) {
+      // Handle loss of tracking
+    }
+  }
+}
+
+var styles = StyleSheet.create({
+  helloWorldTextStyle: {
+    fontFamily: "Arial",
+    fontSize: 30,
+    color: "#ffffff",
+    textAlignVertical: "center",
+    textAlign: "center"
+  }
+});
