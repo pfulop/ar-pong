77391a760e6aabada77a55f83d211c7cf952bb8b
Frane Antunovic <frane.antunovic@frontmen.nl>
2019-10-23 12:11:10 +0200

    New models and textures

--- js/ARScene.js
+++ js/ARScene.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-
 import {StyleSheet} from 'react-native';
+import GameActions from './GameActions';
+import {GameContextProvider} from './Context/GameContext';
+import {MasterContextProvider} from './Context/MasterContext';
 
 import {
   ViroARScene,
@@ -12,22 +14,12 @@ import {
   ViroARPlane,
   ViroMaterials,
   ViroAmbientLight,
-} from 'react-viro';
+  Viro3DObject
+} from "react-viro";
 
-import GameActions from './GameActions';
-import {GameContextProvider} from './Context/GameContext';
-import {MasterContextProvider} from './Context/MasterContext';
-
-const DIMENSIONS = {
-  playgroundHeight: 0.01,
-  playgroundLength: 0.2,
-  playgroundWidth: 0.5,
-  borderHeight: 0.02,
-  borderLength: 0.01,
-  paddleHeight: 0.01,
-  paddleLength: 0.07,
-  paddleWidth: 0.01,
-};
+const packModel = require("./res/D7-AirHockeyPuck.obj");
+const paddleModel = require("./res/D7-AirHockeyHandle.obj");
+const imageMarker = require("./res/ah.jpg");
 
 export default class ARScene extends Component {
   constructor() {
@@ -36,10 +28,14 @@ export default class ARScene extends Component {
     this.state = {
       paddleAZ: 0,
       paddleBZ: 0,
+      playgroundWidth: 1,
+      playgroundLength: 0.5
     };
 
-    maxZ = () => DIMENSIONS.playgroundLength / 2 - DIMENSIONS.paddleLength / 2;
-    minZ = () => -maxZ();
+    // TODO: Remove paddle mocked movement
+    let paddleLength = parseFloat((this.state.playgroundLength / 3).toFixed(3));
+    maxZ = () => parseFloat((this.state.playgroundLength / 2 - paddleLength / 2).toFixed(3));
+    minZ = () => - maxZ();
 
     setInterval(() => {
       let randomAZ = Math.random() * (maxZ() - minZ()) + minZ();
@@ -50,54 +46,89 @@ export default class ARScene extends Component {
         paddleBZ: randomBZ,
       });
     }, 500);
+    // END of TODO
 
-    // bind 'this' to functions
+    this.paddleA = React.createRef();
+
     this._onInitialized = this._onInitialized.bind(this);
     this._onAnchorFound = this._onAnchorFound.bind(this);
+    this._onPinch = this._onPinch.bind(this);
   }
 
   render() {
+    // Computable dimensions
+    let playgroundLength = this.state.playgroundWidth / 2;
+    let playgroundHeight = this.state.playgroundWidth / 100;
+
+    let borderHeight = playgroundHeight * 4;
+    let borderLength = playgroundHeight * 2;
+    let borderWidth = this.state.playgroundWidth;
+
+    let paddleHeight = playgroundHeight * 2;
+    let paddleLength = parseFloat((playgroundLength / 4).toFixed(3));
+    let paddleWidth = playgroundHeight * 4;
+
+    // Computable positions
+    let borderAZ = parseFloat((playgroundLength / 2 + borderLength / 2).toFixed(3));
+    let borderBZ = - borderAZ;
+
+    let borderY = (borderHeight - playgroundHeight) / 2;
+
+    let paddleAX = parseFloat((this.state.playgroundWidth / 2 - paddleWidth / 2).toFixed(3));
+    let paddleBX = - paddleAX;
+
+    let paddleY = playgroundHeight;
+
     return (
       <GameContextProvider>
         <MasterContextProvider>
-          <ViroARScene onTrackingUpdated={this._onInitialized}>
-            <ViroARImageMarker target={'marker'}>
+            <ViroARScene onTrackingUpdated={this._onInitialized}>
+            <ViroARImageMarker target={"marker"}> 
               <GameActions />
-              <ViroAmbientLight color={'white'} />
+              <ViroAmbientLight color={"#FFFFFF"}/>
               <ViroBox
                 position={[0, 0, 0]}
-                height={DIMENSIONS.playgroundHeight}
-                length={DIMENSIONS.playgroundLength}
-                width={DIMENSIONS.playgroundWidth}
-                materials={['grass']}
-              />
+                height={playgroundHeight}
+                length={playgroundLength}
+                width={this.state.playgroundWidth}
+                materials={["white"]}
+                onPinch={this._onPinch} />
               <ViroBox
-                position={[0, 0, 0.105]}
-                height={DIMENSIONS.borderHeight}
-                length={DIMENSIONS.borderLength}
-                width={DIMENSIONS.playgroundWidth}
-                materials={['white']}
-              />
+                position={[0, borderY, borderAZ]}
+                height={borderHeight}
+                length={borderLength}
+                width={borderWidth}
+                materials={["whiteFilled"]} />
               <ViroBox
-                position={[0, 0, -0.105]}
-                height={DIMENSIONS.borderHeight}
-                length={DIMENSIONS.borderLength}
-                width={DIMENSIONS.playgroundWidth}
-                materials={['white']}
+                position={[0, borderY, borderBZ]}
+                height={borderHeight}
+                length={borderLength}
+                width={borderWidth}
+                materials={["whiteFilled"]} />
+              <Viro3DObject
+                ref={this.paddleA}
+                source={paddleModel}
+                position={[paddleAX, paddleY, this.state.paddleAZ]}
+                materials={["red"]} 
+                type={"OBJ"}
+                rotation={[0, 0, 0]}
+                scale={[.0008, .0008, .0008]}
               />
-              <ViroBox
-                position={[-0.245, 0.01, this.state.paddleAZ]}
-                height={DIMENSIONS.paddleHeight}
-                length={DIMENSIONS.paddleLength}
-                width={DIMENSIONS.paddleWidth}
-                materials={['red']}
+              <Viro3DObject
+                source={packModel}
+                position={[0, 0, 0,]}
+                materials={["red"]} 
+                type={"OBJ"}
+                rotation={[0, 0, 0]}
+                scale={[.001, .001, .001]}
               />
-              <ViroBox
-                position={[0.245, 0.01, this.state.paddleBZ]}
-                height={DIMENSIONS.paddleHeight}
-                length={DIMENSIONS.paddleLength}
-                width={DIMENSIONS.paddleWidth}
-                materials={['red']}
+              <Viro3DObject
+                source={paddleModel}
+                position={[paddleBX, paddleY, this.state.paddleBZ]}
+                materials={["red"]} 
+                type={"OBJ"}
+                rotation={[0, 0, 0]}
+                scale={[.0008, .0008, .0008]}
               />
             </ViroARImageMarker>
           </ViroARScene>
@@ -116,33 +147,50 @@ export default class ARScene extends Component {
   _onAnchorFound() {
     alert('Anchor found!');
   }
+
+  _onPinch(pinchState, scaleFactor, source) {
+
+    if (pinchState === 1) {
+      this.baseScaleFactor = scaleFactor;
+    }
+
+    if (pinchState === 2) {
+      let diff = scaleFactor - this.baseScaleFactor;
+      let playgroundWidth =  this.state.playgroundWidth + diff;
+      playgroundWidth = playgroundWidth < 0.2 ? 0.2 : playgroundWidth;
+      this.setState({
+        playgroundLength: parseFloat((playgroundWidth / 2).toFixed(3)),
+        playgroundWidth,
+        playgroundHeight: parseFloat((playgroundWidth / 100).toFixed(3))
+      })
+      this.baseScaleFactor = scaleFactor;
+    }
+  }
 }
 
 ViroARTrackingTargets.createTargets({
-  marker: {
-    source: require('./res/ah.jpg'),
-    orientation: 'Up',
-    physicalWidth: 0.08, // real world width in meters
+  "marker" : {
+    source: imageMarker,
+    orientation: "Up",
+    physicalWidth: 0.08 // real world width in meters
   },
 });
 
 ViroMaterials.createMaterials({
-  grass: {
-    lightingModel: 'PBR',
-    diffuseTexture: require('./res/grass.jpg'),
-  },
-  stone: {
-    lightingModel: 'PBR',
-    diffuseTexture: require('./res/stone.jpg'),
-  },
   white: {
-    lightingModel: 'PBR',
-    diffuseColor: 'rgb(231,231,231)',
+    lightingModel: "PBR",
+    diffuseColor: "rgb(231,231,231)",
+    blendMode: 'Add'
+  },
+  whiteFilled: {
+    lightingModel: "PBR",
+    diffuseColor: "rgb(231,231,231)"
   },
   red: {
-    lightingModel: 'PBR',
-    diffuseColor: 'rgb(255,0,0)',
-  },
+    lightingModel: "PBR",
+    diffuseColor: "rgb(255,0,0)",
+    diffuseIntensity: 0.5
+  }
 });
 
 var styles = StyleSheet.create({
