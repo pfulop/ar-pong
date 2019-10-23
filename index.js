aaaf7f7e319941f5ab56ec9fd5053c2ac2d7fb77
Frane Antunovic <frane.antunovic@frontmen.nl>
2019-10-23 14:47:39 +0200

    Fix for Android RN version mismatch

--- index.js
+++ index.js
@@ -2,6 +2,3 @@ import { AppRegistry } from 'react-native';
 import App from './App.js';
 
 AppRegistry.registerComponent('gdays', () => App);
-
-// The below line is necessary for use with the TestBed App
-AppRegistry.registerComponent('ViroSample', () => App);
